e0ffd0fac41393dd33c3d0ba86b553daec6e58d5
kartikmehta18 <125860170+kartikmehta18@users.noreply.github.com>
2024-05-05 23:57:35 +0530

    JAVA

--- lecture_5/halfpyra.java
+++ lecture_5/halfpyra.java
@@ -2,6 +2,12 @@ package lecture_5;
 
 public class halfpyra {
     public static void main(String[] args){
-        
+        for(int i=1;i<=4;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+       
     }
 }
