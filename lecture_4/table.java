88b7ade0313e308c5e22342a1b90888e646880eb
kartikmehta18 <125860170+kartikmehta18@users.noreply.github.com>
2024-05-05 15:50:42 +0530

    JAVA

--- lecture_4/table.java
+++ lecture_4/table.java
@@ -1,5 +1,15 @@
 package lecture_4;
 
+import java.util.Scanner;
+
 public class table {
-    
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number table you want");
+        int n= sc.nextInt();
+        for(int i=1 ;i<=10;i++){
+            int mul =n*i;
+            System.out.println(n+"*"+i+"="+mul);
+        }
+    }
 }
