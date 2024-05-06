67723c980ea15dcb7dccf37af0724a5e220e9557
kartikmehta18 <125860170+kartikmehta18@users.noreply.github.com>
2024-05-06 20:08:54 +0530

    JAVA

--- lecture_5/numpyr.java
+++ lecture_5/numpyr.java
@@ -1,9 +1,41 @@
+// package lecture_5;
+
+// import java.util.Scanner;
+
+// public class numpyr {
+//     public static void main(String[] args){
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println("Enter the num till pyramid u  want");
+//         int n =sc.nextInt();
+//         for(int i=1;i<=n;i++){
+//             for(int j=1;j<=i;j++){
+//                 System.out.print(j+" ");
+//             }
+//             System.out.println( );
+//         }
+
+
+//     }
+// }
+
+// reverse
+
 package lecture_5;
+
+import java.util.Scanner;
 
 public class numpyr {
     public static void main(String[] args){
-        int n =1;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the num till pyramid u  want");
+        int n =sc.nextInt();
+        for(int i=n;i>=1;i--){
+            for(int j=1;j<=i;j++){
+                System.out.print(j+" ");
+            }
+            System.out.println( );
+        }
 
-        
+
     }
 }
