e70313a4d39164cf67b4b6c9c2baf636ee47c95c
kartikmehta18 <125860170+kartikmehta18@users.noreply.github.com>
2024-05-06 20:31:13 +0530

    JAVA

--- lecture_5/floydtry.java
+++ lecture_5/floydtry.java
@@ -1,5 +1,16 @@
 package lecture_5;
 
 public class floydtry {
-    
+    public static void main(String[] args) {
+        int n=5;
+        int number =1;
+
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++){
+        System.out.print(number+" ");
+        number++;
+        }
+        System.out.println();
+    }
+}
 }
---
+++ lecture_5/tri0_1.java
@@ -0,0 +1,8 @@
+package lecture_5;
+
+public class tri0_1 {
+    public static void main(String[] args) {
+        int n=5;
+        for()
+    }
+}
