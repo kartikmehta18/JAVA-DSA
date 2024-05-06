521fb528fe5b7be401cfe6191075fcc4ce725074
kartikmehta18 <125860170+kartikmehta18@users.noreply.github.com>
2024-05-06 19:14:52 +0530

    JAVA

--- lecture_5/halfpyra.java
+++ lecture_5/halfpyra.java
@@ -1,13 +1,48 @@
+// package lecture_5;
+
+// public class halfpyra {
+//     public static void main(String[] args){
+//         for(int i=1;i<=4;i++){
+//             for(int j=1;j<=i;j++){
+//                 System.out.print("*");
+//             }
+//             System.out.println();
+//         }
+       
+//     }
+// }
+
+//invert
+// package lecture_5;
+
+// public class halfpyra {
+//     public static void main(String[] args){
+//         for(int i=4;i>=1;i--){
+//             for(int j=1;j<=i;j++){
+//                 System.out.print("*");
+//             }
+//             System.out.println();
+//         }
+       
+//     }
+// }
+
+
+//invert by 180
 package lecture_5;
 
 public class halfpyra {
     public static void main(String[] args){
         for(int i=1;i<=4;i++){
-            for(int j=1;j<=i;j++){
-                System.out.print("*");
+            for(int j=1;j<=4-i;j++){
+                System.out.print(" ");
+            }
+            for(int j=1 ;j<=i ;j++){
+            System.out.print("*");
             }
             System.out.println();
         }
        
     }
 }
+
---
+++ lecture_5/numpyr.java
@@ -0,0 +1,5 @@
+package lecture_5;
+
+public class numpyr {
+    
+}
