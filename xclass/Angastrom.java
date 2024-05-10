8bc59efb05e8b301af591f6c6a75a3eb518be243
kartikmehta18 <125860170+kartikmehta18@users.noreply.github.com>
2024-05-10 14:37:11 +0530

    JAVA

--- xclass/Angastrom.java
+++ xclass/Angastrom.java
@@ -5,11 +5,25 @@ import java.util.Scanner;
 public class Angastrom {
 
     public static void Angastrom(int num){
+        int res =0;
+        int n,r;
+        for(int i=1;i<=num;i++){
+            n=i;
+            res=0;
+            while (n!=0) {
+                r=n%10;
+                res=res*10+r*r*r;
+                n=n/10;
+            }
+            if (i==res){
+                System.out.println(i+" ");
+            }
 
+        }
     }
     public static void main(String[] args) {
          Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the Angastrom number");
+        System.out.println("Enter the number till Angastrom number you want");
         int num = sc.nextInt();
         Angastrom(num);
     }
