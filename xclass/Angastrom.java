f16704bfdb29ebb7a4730245ed4649e5e2868ab8
kartikmehta18 <125860170+kartikmehta18@users.noreply.github.com>
2024-05-10 14:34:06 +0530

    JAVA

--- xclass/Angastrom.java
+++ xclass/Angastrom.java
@@ -1,11 +1,16 @@
 package xclass;
 
+import java.util.Scanner;
+
 public class Angastrom {
 
-    public static void Angastrom(int a){
+    public static void Angastrom(int num){
 
     }
     public static void main(String[] args) {
-        
+         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the Angastrom number");
+        int num = sc.nextInt();
+        Angastrom(num);
     }
 }
