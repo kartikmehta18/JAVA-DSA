24ef9c7ea677d5b7e5a668a60270a93d76d4f64c
kartikmehta18 <125860170+kartikmehta18@users.noreply.github.com>
2024-05-13 19:15:24 +0530

    JAVA

--- lecture_7/factorial.java
+++ lecture_7/factorial.java
@@ -3,22 +3,37 @@ package lecture_7;
 import java.util.Scanner;
 
 public class factorial {
-
-    public static int factorialNum(int a){
-        if(a == 1)
-        {
-            return 1;
-        }
-        else
-        {
-            return a*factorialNum(a-1);
-        }
+    //RECURSION
+    // public static int factorialNum(int a){
+    //     if(a == 1)
+    //     {
+    //         return 1;
+    //     }
+    //     else
+    //     {
+    //         return a*factorialNum(a-1);
+    //     }
     
-    }
+    // }
+
+    public static void factorialNum(int a){
+        if (a<0){
+            System.out.println("invalid num");
+            return;
+        }
+        int factoriall =1;
+
+        for(int i=a;i>=1;i--){
+          factoriall=factoriall*i;
+        }
+        System.out.println(factoriall);
+      }
+       
     public static void main(String[] args) {
          Scanner sc = new Scanner(System.in);
          System.out.println("Enter the number of factorial u want");
          int a=sc.nextInt();
-         System.out.println(factorialNum(a));
+         factorialNum(a);
+         
     }
 }
