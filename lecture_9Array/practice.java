fb8270dbd02b6171a329fd68eecd2484002c6670
kartikmehta18 <125860170+kartikmehta18@users.noreply.github.com>
2024-05-15 20:04:35 +0530

    JAVA

--- lecture_9Array/practice.java
+++ lecture_9Array/practice.java
@@ -1,5 +1,26 @@
 package lecture_9Array;
 
+import java.util.Scanner;
+
 public class practice {
-    
+    public static void main(String[] args) {
+         Scanner sc= new Scanner(System.in);
+        System.out.println("Enter the size of array");
+        int size =sc.nextInt();
+
+        int number[] =new int [size];
+        //ip
+        for(int i=0;i<size;i++){
+            number[i]=sc.nextInt();
+        }
+        System.out.println("x found index:");
+        //op
+        int x=sc.nextInt();
+       
+        for(int i=0;i<number.length;i++){
+            if(number[i] == x){
+            System.out.println("x found at index:"+i);
+            }
+        }
+    }
 }
