6e4c529635317939a66108e700e1260359140a2e
kartikmehta18 <125860170+kartikmehta18@users.noreply.github.com>
2024-05-20 16:01:17 +0530

    java

--- lecture_9Array/sort.java
+++ lecture_9Array/sort.java
@@ -1,29 +1,66 @@
 package lecture_9Array;
 
-import java.util.Scanner;
+// import java.util.Scanner;
+
+// public class sort {
+//     public static void main(String[] args) {
+//         Scanner sc=new Scanner(System.in);
+//         int size = sc.nextInt();
+//         int number[] = new int[size];
+
+//         //input
+//         for(int i=0; i<size;i++){
+//             number[i] =sc.nextInt();
+//         }
+//         boolean isAscending = true;
+//         for(int i=0; i<number.length-1; i++) { // NOTICE numbers.length - 1 as termination condition
+//             if(number[i] > number[i+1]) { // This is the condition for descending order
+//                 isAscending = false;
+//             }
+//         }
+ 
+ 
+//         if(isAscending) {
+//             System.out.println("The array is sorted in ascending order");
+//         } else {
+//             System.out.println("The array is not sorted in ascending order");
+//         }
+ 
+//     }
+// }
+
+
+import java.util.*;
+
 
 public class sort {
-    public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        int size = sc.nextInt();
-        int number[] = new int[size];
+   public static void main(String args[]) {
+      Scanner sc = new Scanner(System.in);
+      int size = sc.nextInt();
+      int numbers[] = new int[size];
 
-        //input
-        for(int i=0; i<size;i++){
-            number[i] =sc.nextInt();
+
+      //input
+      for(int i=0; i<size; i++) {
+          numbers[i] = sc.nextInt();
+      }
+
+
+      boolean isAscending = true;
+      for(int i=0; i<numbers.length-1; i++) { // NOTICE numbers.length - 1 as termination condition
+        if(numbers[i] > numbers[i+1]) { // This is the condition for descending order
+            isAscending = false;
         }
-        for(int i=0; i<number.length-1; i++) { // NOTICE numbers.length - 1 as termination condition
-            if(number[i] > number[i+1]) { // This is the condition for descending order
-                isAscending = false;
-            }
-        }
- 
- 
-        if(isAscending) {
-            System.out.println("The array is sorted in ascending order");
-        } else {
-            System.out.println("The array is not sorted in ascending order");
-        }
- 
     }
+
+
+    if(isAscending) {
+        System.out.println("The array is sorted in ascending order");
+    } else {
+        System.out.println("The array is not sorted in ascending order");
+    }
+   
 }
+}
+
+
