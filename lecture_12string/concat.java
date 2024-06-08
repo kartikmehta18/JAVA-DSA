4e3676c9cfe0c1105e01f9db3871c753ad1723ed
kartikmehta18 <125860170+kartikmehta18@users.noreply.github.com>
2024-06-09 01:54:52 +0530

    java

---
+++ lecture_12string/concat.java
@@ -0,0 +1,37 @@
+package lecture_12string;
+
+import java.util.Scanner;
+
+public class concat {
+    public static void main(String args[]){
+ Scanner sc = new Scanner(System.in);
+        System.out.println("entr your name and lastname");
+        String name =sc.next(); //singel word
+        String name1 =sc.next(); // whoel line
+        String fullName = name+" "+name1;
+        System.out.println(fullName);
+        int str = fullName.length();
+        System.out.println(str);
+        for(int i=0; i<fullName.length();i++){
+
+        System.out.println(fullName.charAt(i));
+         }// print char at index
+// now for comparing two name
+System.out.println("Enter 2 name for comparew");
+String name2 =sc.next();
+String name3 =sc.next();
+            // if (name2.compareTo(name3)==0){
+            //     System.out.println("strings are equal");
+            // }else{
+            //     System.out.println("string not equal");
+            // }
+            //OR
+            if (name2 == name3){
+                System.out.println("strings are equal");
+            }else{
+                System.out.println("string not equal");
+            }
+       String k= fullName.substring(3); //parsint
+System.out.println(k);
+    }
+}
--- lecture_12string/strings.java
+++ lecture_12string/strings.java
@@ -6,7 +6,8 @@ public class strings {
     public static void main (String args[]){
         Scanner sc = new Scanner(System.in);
         System.out.println("entr your name");
-        String name =sc.next();
+        String name =sc.next(); //singel word
+        String name1 =sc.nextLine(); // whoel line
         System.out.println(name);
 
         // String name ="tony";
