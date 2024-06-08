8ece5422e63619b10c880473ea8bb6cf5bf84418
kartikmehta18 <125860170+kartikmehta18@users.noreply.github.com>
2024-06-09 00:54:50 +0530

    java

---
+++ lecture_12string/strings.java
@@ -0,0 +1,16 @@
+package lecture_12string;
+
+import java.util.Scanner;
+
+public class strings {
+    public static void main (String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("entr your name");
+        String name =sc.next();
+        System.out.println(name);
+
+        // String name ="tony";
+        // String fullName ="stark";
+
+    }
+}
