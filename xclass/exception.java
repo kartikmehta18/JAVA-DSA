04301b97c12638f9c51e6bf39dc3f03a682e0e3f
kartikmehta18 <125860170+kartikmehta18@users.noreply.github.com>
2024-10-09 22:59:33 +0530

    java

--- xclass/exception.java
+++ xclass/exception.java
@@ -4,29 +4,26 @@ import java.util.Scanner;
 
 public class exception {
     public static void main(String[] args) {
-        int [] marks = new int [3] ;
-        marks[0]=7;
-        marks[1]=56;
-        marks[2]=6;
-        Scanner sc =new Scanner(System.in);
+        int[] marks = new int[3];
+        marks[0] = 7;
+        marks[1] = 56;
+        marks[2] = 6;
+        Scanner sc = new Scanner(System.in);
         System.out.println("enter the arr index");
-        int in =sc.nextInt();
+        int in = sc.nextInt();
         System.out.println("enter the number for divide");
-        int number =sc.nextInt();
-        try{
-            System.out.println(" The value of aarr index enterd id:"+marks[in]);
-            System.out.println("the value of "+marks[in]+"/"+number+"is"+" "+marks[in]/number );
+        int number = sc.nextInt();
+        try {
+            System.out.println(" The value of aarr index enterd id:" + marks[in]);
+            System.out.println("the value of " + marks[in] + "/" + number + "is" + " " + marks[in] / number);
 
-        }
-        catch(ArithmeticException e){
+        } catch (ArithmeticException e) {
             System.out.println("ArithmeticException occur !");
             System.out.println(e);
-        }
-        catch(ArrayIndexOutOfBoundsException e){
+        } catch (ArrayIndexOutOfBoundsException e) {
             System.out.println("ArrayIndexOutOfBoundsExceptionoccur!!");
             System.out.println(e);
-        }
-        catch(Exception e){
+        } catch (Exception e) {
             System.out.println("some acception occur!");
             System.out.println(e);
         }
--- xclass/primenum.java
+++ xclass/primenum.java
@@ -1,10 +1,11 @@
 package xclass;
 
 public class primenum {
-    public static void primeNum(int num){
-        
+    public static void primeNum(int num) {
+
     }
+
     public static void main(String[] args) {
-        
+
     }
 }
