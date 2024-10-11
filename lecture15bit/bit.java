331423ab382e3cbef0542cf396bc0d09ed15b5bb
kartikmehta18 <125860170+kartikmehta18@users.noreply.github.com>
2024-10-11 22:09:11 +0530

    java

--- lecture15bit/bit.java
+++ lecture15bit/bit.java
@@ -2,13 +2,15 @@ package lecture15bit;
 
 public class bit {
     public static void main(String[] args) {
-        int n=5; // 
-        int pos= 3;
-        int bitmask =1<<pos;
-        if((bitmask & n)==0){
+        int n = 5; // Input number 0101
+        int pos = 3; // Position of the bit to check (0-indexed)
+        int bitmask = 1 << pos; // Create a bitmask with a 1 at the given position
+
+        // Check if the bit at the given position is 0 or 1
+        if ((bitmask & n) == 0) {
             System.out.println("bit was zero");
-        }else[
-            System.out.println('bit was one');
-        ]
+        } else {
+            System.out.println("bit was one");
+        }
     }
 }
