449495b4c2c58a420ca44897e264a959de22e76d
kartikmehta18 <125860170+kartikmehta18@users.noreply.github.com>
2024-10-12 00:59:46 +0530

    java

---
+++ lecture16/bubblesort.java
@@ -0,0 +1,16 @@
+package lecture16;
+
+public class bubblesort {
+    public static void main(String[] args) {
+        int arr [] ={7,8,3,1,2}
+
+       // bubblesort
+
+       for(int i=0; i<arr.length-1; i++){
+        for(int j=0; j<arr.length-i-1; j++){
+
+        }
+
+       }
+    }
+}
