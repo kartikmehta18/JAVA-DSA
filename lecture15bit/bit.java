89406ca42b0c2c4a5cf6b808e4efc49c32c93137
kartikmehta18 <125860170+kartikmehta18@users.noreply.github.com>
2024-10-11 20:08:18 +0530

    java

---
+++ lecture15bit/bit.java
@@ -0,0 +1,14 @@
+package lecture15bit;
+
+public class bit {
+    public static void main(String[] args) {
+        int n=5; // 
+        int pos= 3;
+        int bitmask =1<<pos;
+        if((bitmask & n)==0){
+            System.out.println("bit was zero");
+        }else[
+            System.out.println('bit was one');
+        ]
+    }
+}
--- lecture_13strBuilder/reversestr.java
+++ lecture_13strBuilder/reversestr.java
@@ -5,7 +5,7 @@ public class reversestr {
         StringBuilder sb = new StringBuilder("hello");
         for(int i=0;i<=sb.length()/2;i++){
             int front =i;
-            int back= sb.length()-1-i;
+            int back= sb.length()-1-i; //5-1-0 =>4 
             char frontChar = sb.charAt(front);
             char backChar = sb.charAt(back);
 
