bac92207fdcb9e82722c5d0c1ba8bd44269756ff
kartikmehta18 <125860170+kartikmehta18@users.noreply.github.com>
2024-10-12 19:33:52 +0530

    selectionsort

---
+++ lecture16/selectionsort.java
@@ -0,0 +1,30 @@
+package lecture16;
+
+public class selectionsort {
+
+    public static void printArray(int arr[]){
+        System.out.println("sorted  array is this:");
+        for(int i=0 ; i< arr.length;i++){
+            System.out.println(arr[i] +" ");
+        }
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 7, 8, 3, 1, 2 };
+
+        // selection
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            int smallest = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[smallest] > arr[j]) {
+                    smallest = j;
+                }
+            }
+            int temp = arr[smallest];
+            arr[smallest] = arr[i];
+            arr[i] = temp;
+        }
+        printArray(arr);
+    }
+}
