1585ca53c0950493e2c322136c20572efa37a354
kartikmehta18 <125860170+kartikmehta18@users.noreply.github.com>
2024-10-19 22:46:31 +0530

    java

--- lecture_13strBuilder/buil.java
+++ lecture_13strBuilder/buil.java
@@ -4,21 +4,20 @@ public class buil {
     public static void main(String[] args) {
         StringBuilder str = new StringBuilder("Tonny");
         System.out.println(str);
-        //char at index 0
+        // char at index 0
         System.out.println(str.charAt(0));
 
-        //replace str val at indx
+        // replace str val at indx
         str.setCharAt(0, 'p');
         System.out.println(str);
 
-    //insert value
-    str.insert(0,'s');
-    System.out.println(str);
+        // insert value
+        str.insert(0, 's');
+        System.out.println(str);
 
-    //delete
-    str.delete(2,3);
-    System.out.println(str);
+        // delete
+        str.delete(2, 3);
+        System.out.println(str);
 
-    
     }
 }
--- lecture_13strBuilder/upends.java
+++ lecture_13strBuilder/upends.java
@@ -1,7 +1,7 @@
 package lecture_13strBuilder;
 
 public class upends {
-    public static void main (String args[]){
+    public static void main(String args[]) {
         StringBuilder sb = new StringBuilder("h");
         sb.append("e");
         sb.append("l");
