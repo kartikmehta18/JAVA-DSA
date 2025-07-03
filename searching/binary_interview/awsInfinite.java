e38177b447c39c4dff45293b19fe02a19c5d0382
kartikmehta18 <125860170+kartikmehta18@users.noreply.github.com>
2025-07-04 00:14:32 +0530

    leet 34

--- searching/binary_interview/awsInfinite.java
+++ searching/binary_interview/awsInfinite.java
@@ -2,7 +2,37 @@ package searching.binary_interview;
 
 public class awsInfinite {
     public static void main(String[] args) {
-        
+
     }
-    
+
+    static int ans(int[] arr, int target) {
+        // first we find the range
+        int start = 0;
+        int end = 1;
+
+        // condition for the target lie in the range
+        while ( target > arr[mid]) {
+            int newStart= end+1
+            int
+            
+        }
+
+    }
+
+    static int binarySearch(int[] arr, int target, int start) {
+
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            // int mid = start+(end-start)/2;
+
+            if (target < arr[mid]) {
+                end = mid - 1;
+            } else if (target > arr[mid]) {
+                start = mid + 1;
+            } else {
+                return mid;
+            }
+        }
+        return -1;
+    }
 }
